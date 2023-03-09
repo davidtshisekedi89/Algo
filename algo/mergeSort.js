e69fb87e4979ee6c141f9ecee14d6b7969a56367
davidtshisekedi89 <davidallstar89@gmail.com>
2023-03-08 20:08:16 -0500

    initial commit

---
+++ algo/mergeSort.js
@@ -0,0 +1,106 @@
+/* 
+  Stable sort.
+
+  Visualization:
+  https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/visualize/
+
+  Time Complexity
+    - Best case: O(n log(n)) linearithmic.
+    - Average case: O(n log(n)) linearithmic.
+    - Worst case: O(n log(n)) linearithmic.
+
+  Space: O(n) linear
+
+  steps:
+    1. create a merge function to merge two already sorted arrays into a single
+        sorted array.
+      - you do NOT need to work in place, ok to use a new array
+    2. create mergeSort function to sort the provided array
+      - split the array in half and recursively merge the halves using the
+          previously created merge function.
+      - splitting of arrays stops when array can no longer be split.
+      - an array of 1 item is by definition sorted, so two arrays of 1 item
+          can therefore be merged into a sorted array.
+*/
+
+// merge
+const sortedA1 = [];
+const sortedB1 = [];
+const expectedMerge1 = [];
+
+const sortedA2 = [5];
+const sortedB2 = [2];
+const expectedMerge2 = [2, 5];
+
+const sortedA3 = [3];
+const sortedB3 = [2, 3, 4, 7];
+const expectedMerge3 = [2, 3, 3, 4, 7];
+
+const sortedA4 = [1, 2, 4, 5, 6, 9];
+const sortedB4 = [3, 7, 8, 10];
+const expectedMerge4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Merges two already sorted arrays into a new sorted array.
+ * - Time: O(n + m) -> O(n) linear n = left.length, m = right.length.
+ *    Every item from each array is visited once.
+ * - Space: O(n + m) -> O(n) linear.
+ * @param {Array<number>} left
+ * @param {Array<number>} right
+ * @returns {Array<number>} A new sorted array containing all the elements of
+ *    both given halves.
+ */
+function merge(left = [], right = []) {
+  let result = [];
+  let indexLeft = 0;
+  let indexRight = 0;
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      result.push(left[indexLeft]);
+      indexLeft++;
+    } else {
+      result.push(right[indexRight]);
+      indexRight++;
+    }
+  }
+
+  // in case one of the arrays has remaining items due to unequal lengths, all of those can be added
+  // while (indexLeft < left.length) {
+  //   result.push(left[indexLeft]);
+  //   indexLeft++;
+  // }
+
+  // while (indexRight < right.length) {
+  //   result.push(right[indexRight]);
+  //   indexRight++;
+  // }
+
+  return result;
+  // return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+
+  // one liner version of adding in any left over items
+}
+
+/**
+ * Creates a new sorted array based on the given numbers being recursively split
+ * and merged.
+ * Best: O(n log(n)) linearithmic.
+ * Avg: O(n log(n)) linearithmic.
+ * Worst: O(n log(n)) linearithmic.
+ * @param {Array<number>} numbers
+ * @returns {Array<number>} A New sorted array.
+ */
+function mergeSort(numbers = []) {
+  if (numbers.length === 1) {
+    // return once we hit an array with a single item
+    return numbers;
+  }
+
+  const middleIdx = Math.floor(numbers.length / 2);
+  const left = numbers.slice(0, middleIdx);
+  const right = numbers.slice(middleIdx);
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+  return merge(sortedLeft, sortedRight);
+}
