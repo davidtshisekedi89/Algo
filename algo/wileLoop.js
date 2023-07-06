90712ecd9605f4b0ae6fec5174fd4843b24adbfc
David T <davidallstar89@gmail.com>
2023-07-06 15:40:31 -0500

    innitial commit

---
+++ algo/wileLoop.js
@@ -0,0 +1,16 @@
+// Let's try getting a while loop to work by pushing values to an array.
+
+// Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+
+// Setup
+
+const myArray = [];
+
+let i = 5;
+while(i > -1){
+  myArray.push(i);
+  i--;
+}
+
+console.log(myArray)
+
